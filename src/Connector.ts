ded5dcd7d1c3584df2371e5e8bfb0ccdc9136738
Tamino Martinius <dev@zaku.eu>
2018-09-03 16:59:10 +0200

    Start with postgres implementation

--- src/Connector.ts
+++ src/Connector.ts
@@ -1,12 +1,46 @@
-export abstract class Connector {
-  constructor(public tableName: string) { }
+import { Connector as AbstractConnector } from '@nextcode/migrator';
+import { Pool } from 'pg';
 
-  public abstract async tableExists(): Promise<boolean>;
-  public abstract async createTable(): Promise<void>;
-  public abstract async dropTable(): Promise<void>;
-  public abstract async getMigrationKeys(): Promise<string[]>;
-  public abstract async insertMigration(key: string): Promise<void>;
-  public abstract async deleteMigrations(key: string): Promise<void>;
+export class Connector extends AbstractConnector {
+  private pool: Pool;
+
+  constructor(public tableName: string) {
+    super(tableName);
+    this.pool = new Pool();
+  }
+
+  public async tableExists(): Promise<boolean> {
+    const result = await this.pool.query(
+      `
+        SELECT * FROM "information_schema"."tables"
+        WHERE "table_schema" = current_schema()
+          AND "table_name" = $1'
+      `,
+      [this.tableName],
+    );
+    return result.rowCount > 0;
+  }
+
+  public async createTable(): Promise<void> {
+
+  }
+
+  public async dropTable(): Promise<void> {
+
+  }
+
+  public async getMigrationKeys(): Promise<string[]> {
+
+  }
+
+  public async insertMigration(key: string): Promise<void> {
+
+  }
+
+  public async deleteMigrations(key: string): Promise<void> {
+
+  }
+
 }
 
 export default Connector;
