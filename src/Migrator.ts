8f273613037abcbf7c4b66be2b747b62fc6c0b71
Tamino Martinius <dev@zaku.eu>
2018-09-03 11:34:06 +0200

    Add init method to Migrator

--- src/Migrator.ts
+++ src/Migrator.ts
@@ -8,6 +8,24 @@ export class Migrator {
   lastMigration: string | undefined;
 
   constructor(public connector: Connector) { }
+
+  async init(): Promise<void> {
+    if (this.initStatus === true) return Promise.resolve();
+    if (this.initStatus === false) {
+      return this.initStatus = new Promise(async (resolve, reject) => {
+        const migrationTableExists = await this.connector.tableExists();
+        if (!migrationTableExists) await this.connector.createTable();
+        const migrationKeys = await this.connector.getMigrationKeys();
+        for (const key of migrationKeys) {
+          this.migrationStatus[key] = true;
+          this.migrationPromises[key] = Promise.resolve();
+          this.lastMigration = key;
+        }
+        resolve();
+      });
+    }
+    return this.initStatus;
+  }
 }
 
 export default Migrator;
