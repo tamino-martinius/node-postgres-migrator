320bd66c5806449f167f27d8e860507d3c8f41d3
Tamino Martinius <dev@zaku.eu>
2018-09-04 10:46:23 +0200

    Create Database before and delete Database after tests

--- src/__tests__/Connector.spec.ts
+++ src/__tests__/Connector.spec.ts
@@ -1,21 +1,54 @@
 import { context } from './types';
+import { Pool } from 'pg';
 import {
   Migrator,
   Dict,
 } from '..';
+import { Connector } from '../Connector';
 
-process.env.PGDATABASE = 'testcode';
+if (!process.env.PGDATABASE) process.env.PGDATABASE = 'testcode';
 
-describe('Migrator', () => {
-  context('when no test is present', {
-    definitions() {
-      // overwrites
-    },
-    tests() {
-      it('still passes', () => {
-        expect(true).toBeTruthy();
-      });
-    },
+beforeAll(async () => {
+  const pool = new Pool({ database: 'postgres' });
+  const result = await pool.query({
+    name: 'migrator--test--database-exists',
+    text: `
+      SELECT 1
+      FROM pg_database
+      WHERE datname = '${process.env.PGDATABASE}'
+    `,
+    values: [],
+  });
+  if (result.rows.length === 0) {
+    await pool.query({
+      name: 'migrator--test--create-database',
+      text: `CREATE DATABASE "${process.env.PGDATABASE}"`,
+      values: [],
+    });
+  }
+  await pool.end();
+});
+
+afterAll(async () => {
+  const pool = new Pool({ database: 'postgres' });
+  const result = await pool.query({
+    name: 'migrator--test--database-exists',
+    text: `
+      SELECT 1
+      FROM pg_database
+      WHERE datname = '${process.env.PGDATABASE}'
+    `,
+    values: [],
+  });
+  if (result.rows.length > 0) {
+    await pool.query({
+      name: 'migrator--test--drop-database',
+      text: `DROP DATABASE "${process.env.PGDATABASE}"`,
+      values: [],
+    });
+  }
+  await pool.end();
+});
   });
 
 });
