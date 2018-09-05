f4d738cbc518932e0222ee307f075a83091cca49
Tamino Martinius <dev@zaku.eu>
2018-09-05 21:46:41 +0200

    Start with class for cli

---
+++ src/CLI.ts
@@ -0,0 +1,14 @@
+import { resolve, basename } from 'path';
+import {
+  readdirSync,
+  existsSync,
+  mkdirSync,
+} from 'fs';
+// import { Migration } from '../dist';         
+
+export class CLI {
+  folder: string;
+
+}
+
+export default CLI;
