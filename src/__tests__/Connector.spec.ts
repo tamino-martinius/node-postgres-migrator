12f6dc518f97f171f5b350694c5e17afe2202e78
Tamino Martinius <dev@zaku.eu>
2018-09-07 18:47:49 +0200

    Use create and drop database from connector

--- src/__tests__/Connector.spec.ts
+++ src/__tests__/Connector.spec.ts
@@ -1,74 +1,35 @@
 import { context } from './types';
 import { Pool } from 'pg';
 import {
-  Migrator,
-  Dict,
+  Connector,
+  Migration,
 } from '..';
-import { Connector } from '../Connector';
 
 if (!process.env.PGDATABASE) process.env.PGDATABASE = 'testcode';
 
-beforeAll(
-  async () => {
-    const pool = new Pool({ database: 'postgres' });
-    const result = await pool.query({
-      name: 'migrator--test--database-exists',
-      text: `
-        SELECT 1
-        FROM pg_database
-        WHERE datname = '${process.env.PGDATABASE}'
-      `,
-      values: [],
-    });
-    if (result.rows.length === 0) {
-      await pool.query({
-        name: 'migrator--test--create-database',
-        text: `CREATE DATABASE "${process.env.PGDATABASE}"`,
-        values: [],
-      });
-    }
-    await pool.end();
-  },
-  30_000,
-);
+let connector: Connector | undefined;
+let tableName: string | undefined;
+const subject = () => {
+  if (connector !== undefined) connector.disconnect();
+  return connector = new Connector(tableName);
+};
+
+beforeAll(async () => {
+  await subject().createDatabase();
+});
 
 afterAll(async () => {
-  const pool = new Pool({ database: 'postgres' });
-  const result = await pool.query({
-    name: 'migrator--test--database-exists',
-    text: `
-      SELECT 1
-      FROM pg_database
-      WHERE datname = '${process.env.PGDATABASE}'
-    `,
-    values: [],
-  });
-  if (result.rows.length > 0) {
-    await pool.query({
-      name: 'migrator--test--drop-database',
-      text: `DROP DATABASE "${process.env.PGDATABASE}"`,
-      values: [],
-    });
+  await subject().dropDatabase();
+});
+
+afterEach(async () => {
+  if (connector !== undefined) {
+    await connector.dropTable();
+    await connector.disconnect();
   }
-  await pool.end();
 });
 
 describe('Connector', () => {
-  let tableName: string | undefined;
-  let connector: Connector | undefined;
-
-  const subject = () => {
-    if (connector !== undefined) connector.disconnect();
-    return connector = new Connector(tableName);
-  };
-
-  afterEach(async () => {
-    if (connector !== undefined) {
-      await connector.dropTable();
-      await connector.disconnect();
-    }
-  });
-
   describe('#new', () => {
     context('when tableName is not present', {
       definitions() {
