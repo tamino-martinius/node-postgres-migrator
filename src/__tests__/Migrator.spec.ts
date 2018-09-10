2d477e1c214e8f3fe8acc65fd3b664697f1852f0
Tamino Martinius <dev@zaku.eu>
2018-09-10 15:02:31 +0200

    Start with tests for Migrator

--- src/__tests__/Migrator.spec.ts
+++ src/__tests__/Migrator.spec.ts
@@ -9,6 +9,7 @@ if (!process.env.PGDATABASE) process.env.PGDATABASE = 'testcode';
 
 let lastMigrator: Migrator | undefined;
 const migrator = () => {
+  console.log('migrator');
   if (lastMigrator) lastMigrator.connector.disconnect();
   return lastMigrator = new Migrator(new Connector());
 };
@@ -20,7 +21,9 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
-  if (lastMigrator) lastMigrator.connector.disconnect();
+  console.log('afterAll');
+  if (lastMigrator) await lastMigrator.connector.disconnect();
+  console.log('after disconnect');
   const connector = new Connector();
   await connector.dropDatabase();
   await connector.disconnect();
@@ -77,6 +80,28 @@ describe('Migrator', () => {
       },
     });
 
+    context('when migration throws error', {
+      definitions() {
+        migrations = [
+          {
+            key: 'test-1',
+            up: () => { throw 'error'; },
+            down: jest.fn(),
+          },
+        ];
+      },
+      tests() {
+        it('throws error', async () => {
+          try {
+            await subject();
+          } catch (error) {
+            return expect(error).toBeDefined();
+          }
+          expect(false).toBeTruthy(); // not expected to reach
+        });
+      },
+    });
+
     context('when migrations parent build infinite loop', {
       definitions() {
         migrations = [
@@ -96,6 +121,79 @@ describe('Migrator', () => {
       },
       tests() {
         it('throws error', async () => {
+          try {
+            await subject();
+          } catch (error) {
+            return expect(error).toBeDefined();
+          }
+          expect(false).toBeTruthy(); // not expected to reach
+        });
+      },
+    });
+  });
+
+  describe('#up', () => {
+    let migration: Migration = {
+      key: 'test-1',
+      up: jest.fn(),
+      down: jest.fn(),
+    };
+
+    const subject = () => migrator().up(migration);
+
+    it('does not throw error', async () => {
+      expect(subject).not.toThrowError();
+    });
+
+    context('when migration is present', {
+      definitions() {
+        migration = {
+          key: 'test-up-1',
+          up: jest.fn(),
+          down: jest.fn(),
+        };
+      },
+      tests() {
+        it('applies migration', async () => {
+          const fn = migration.up;
+          await subject();
+          expect(fn).toBeCalled();
+        });
+      },
+    });
+
+    context('when migration has invalid parent', {
+      definitions() {
+        migration = {
+          parent: ['test-0'],
+          key: 'test-up-2',
+          up: jest.fn(),
+          down: jest.fn(),
+        };
+      },
+      tests() {
+        it('throws error', async () => {
+          try {
+            await subject();
+          } catch (error) {
+            return expect(error).toBeDefined();
+          }
+          expect(false).toBeTruthy(); // not expected to reach
+        });
+      },
+    });
+
+    context('when migration throws error', {
+      definitions() {
+        migration = {
+          key: 'test-up-3',
+          up: () => { throw 'error'; },
+          down: jest.fn(),
+        };
+      },
+      tests() {
+        it.only('throws error', async () => {
+          console.log('spec');
           try {
             await subject();
           } catch (error) {
